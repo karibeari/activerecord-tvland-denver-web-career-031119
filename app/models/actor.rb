bd2ef316bc17e03a2a684bc053f1b70debf4aafa
github username <github email address>
2019-03-24 19:14:12 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
+
+  def list_roles
+    self.characters.map {|character| "#{character.name} - #{character.show.name}"}
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def build_network(call_letters: )
+    Network.create(call_letters: call_letters).shows << self
+  end
 end
